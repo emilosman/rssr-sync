a598a580a551705ed6698333250398c4ee6b2dae
Emil Oszmanbegovity <emil@emilosman.com>
2025-12-11 12:06:13 +0100

    Revert "Reset"
    This reverts commit e54966308bd0ba50a04b1577034ed348639862ad.

--- cmd/rssr-sync/main.go
+++ cmd/rssr-sync/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/emilosman/rssr-sync/internal/data"
+	"github.com/emilosman/rssr-sync/internal/web"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	lists := &data.Lists{ListIndex: make(map[string]*data.List)}
+	lists.ListIndex["abc"] = &data.List{
+		Ts:     10,
+		ApiKey: "abc",
+		Data:   []byte(`"message": "hello`),
+	}
+
+	server := web.Server(lists)
+
+	fmt.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", server))
+
 }
